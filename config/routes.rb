0902715a5897c7988ec6b5b205247cafbcfc83f7
somecodedude <rickpittman8@gmail.com>
2016-05-26 16:13:08 -0400

    angular test deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
  resources :studies, only: [ :index ]
   devise_for :users
 	root 'dashboard#index'
+
+	get "angular_test", to: "angular_test#index"
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
